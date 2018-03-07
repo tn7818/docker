541bff1905a62abb94c63faf2b9d36f6751b96f2
Qu RongSheng <longdongdong126@126.com>
2018-03-07 17:08:15 +0800

    Add files via upload

--- dockerfiles/nfs/Alpine3.7/nfs-client/container-files/etc/cont-init.d/10_nfs-mount.sh
+++ dockerfiles/nfs/Alpine3.7/nfs-client/container-files/etc/cont-init.d/10_nfs-mount.sh
@@ -1,1 +1,40 @@
+#!/bin/sh
+
+set -e
+
+# FSTYPE
+if [ -z "$FSTYPE" ]; then
+	MOUNTFSTYPE='nfs4'
+else
+	MOUNTFSTYPE=${FSTYPE}
+fi
+
+# MOUNT_TARGET
+if [ -z "$MOUNT_TARGET" ]; then
+	MOUNTTARGET='localhost:/export'
+else
+	MOUNTTARGET=${MOUNT_TARGET}
+fi
+
+# MOUNT_POINT
+if [ -z "$MOUNT_POINT" ]; then
+	MOUNTPOINT='/nfs'
+else
+	MOUNTPOINT=${MOUNT_POINT}
+fi
+
+# MOUNT_OPTIONS
+if [ -z "$MOUNT_OPTIONS" ]; then
+	MOUNTOPTIONS='nolock'
+else
+	MOUNTOPTIONS=${MOUNT_OPTIONS}
+fi
+
+if [ ! -d "$MOUNTPOINT" ]; then
+	mkdir -p ${MOUNTPOINT}
+fi
+
+rpcbind
+
+mount -t ${MOUNTFSTYPE} -o ${MOUNTOPTIONS} ${MOUNTTARGET} ${MOUNTPOINT}
 
