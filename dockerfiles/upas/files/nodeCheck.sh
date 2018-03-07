001e654b9b9cc28f20b974efbf4b15cb54e507d0
Qu RongSheng <longdongdong126@126.com>
2018-03-07 16:58:40 +0800

    Format Changed
    Format Changed

--- dockerfiles/upas/files/nodeCheck.sh
+++ dockerfiles/upas/files/nodeCheck.sh
@@ -8,29 +8,29 @@ if [ -n "$1" ];then
 nodeFile=$1
 fi
 
-echo "#######Node Pre Check Start#######"
+echo "######Node Pre Check Start#######"
 
 echo -e "nodelist \nquit"| $UPAS_HOME/bin/upasadmin -domain $UPAS_DOMAIN -host $UPAS_DAS_SVC_SERVICE_HOST -port $DAS_PORT -u administrator -p $UPAS_PWD|awk 'NR>8 && $6=="N" {print $2}' > $nodeFile
-echo "#######Node Pre Check End#########"
+echo "######Node Pre Check End#########"
 
-echo "####N-Ready Node Delete Start#####"
+echo "######N-Ready Node Delete Start#####"
 
 for i in  `cat $nodeFile`
 do
-echo -e "#Remove $i \n#"
+echo -e "#Remove $i #\n"
 echo -e "remove-node $i \nquit\n"| $UPAS_HOME/bin/upasadmin -domain $UPAS_DOMAIN -host $UPAS_DAS_SVC_SERVICE_HOST -port $DAS_PORT -u administrator -p $UPAS_PWD
 echo -e "\n"
 done
 
-echo "####N-Ready Node Delete End######"
+echo "######N-Ready Node Delete End######"
 
-echo "####Ready Node Register Start####"
+echo "######Ready Node Register Start####"
 
 NodeName=`hostname`
 LocalIP=`/sbin/ifconfig -a|grep inet|grep -v 127.0.0.1|grep -v inet6|awk '{print $2}'|tr -d "addr:"`
 sed -i "s/localhost/$NodeName/g" $UPAS_HOME/nodemanager/upasnm.properties
-echo -e "Register $NodeName - $LocalIP \n#"
+echo -e "Register $NodeName - $LocalIP #\n"
 echo -e "add-java-node $NodeName -host $LocalIP  -port $NM_PORT \nquit\n"| $UPAS_HOME/bin/upasadmin -domain $UPAS_DOMAIN -host $UPAS_DAS_SVC_SERVICE_HOST -port $DAS_PORT -u administrator -p $UPAS_PWD
 echo -e "\n"
 
-echo "####Ready Node Register End######"
+echo "######Ready Node Register End######"
--- dockerfiles/upas/files/serverConfig.sh
+++ dockerfiles/upas/files/serverConfig.sh
@@ -20,24 +20,24 @@ if [ -n "$3" ];then
 serverName=$3
 fi
 
-echo "#######Server Pre Check Start#######"
+echo "######Server Pre Check Start#######"
 
 echo -e "list-servers \nquit"| $UPAS_HOME/bin/upasadmin -domain $UPAS_DOMAIN -host $UPAS_DAS_SVC_SERVICE_HOST -port $DAS_PORT -u administrator -p $UPAS_PWD > $serverFile
-echo "#######Server Pre Check End#########"
+echo "######Server Pre Check End#########"
 
-echo "####Delete The Same Name Server Start#####"
+echo "######Delete The Same Name Server Start#####"
 
 echo -e "#Remove $serverName #\n"
 echo -e "remove-server $serverName \nquit\n"| $UPAS_HOME/bin/upasadmin -domain $UPAS_DOMAIN -host $UPAS_DAS_SVC_SERVICE_HOST -port $DAS_PORT -u administrator -p $UPAS_PWD >> $serverFile
 echo -e "\n"
 
-echo "####Delete The Same Name Server End######"
+echo "######Delete The Same Name Server End######"
 
-echo "####Add New Server Start####"
+echo "######Add New Server Start####"
 
 LocalIP=`/sbin/ifconfig -a|grep inet|grep -v 127.0.0.1|grep -v inet6|awk '{print $2}'|tr -d "addr:"`
 echo -e "Add $serverName - $LocalIP #\n"
 echo -e "add-server $serverName -addr $LocalIP  -target $targetServerName -node `hostname` \nquit\n"| $UPAS_HOME/bin/upasadmin -domain $UPAS_DOMAIN -host $UPAS_DAS_SVC_SERVICE_HOST -port $DAS_PORT -u administrator -p $UPAS_PWD >> $serverFile
 echo -e "\n"
 
-echo "####Add New Server End######"
+echo "######Add New Server End######"
